997d4ebe42fd2740146f82c742370f44930559fc
githubmaster1987 <silverstart1987913@yahoo.com>
2017-07-17 09:49:07 -0700

    Config

---
+++ config.py
@@ -0,0 +1,11 @@
+proxy_username = ""
+proxy_password = ""
+
+proxies = [
+    
+]
+
+username = ""
+userpwd = ""
+
+keywords = [{"Skill":"finance",  "Location":"mumbai", "Experience":5, "Salary":8}]
